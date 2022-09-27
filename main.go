fc9932ad51e07cda1c19ab13bc7d974464d5cfef
tsuem <tsuemotop@gmail.com>
2022-09-27 03:47:27 -0300

    Implementing the rps logic

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"html/template"
 	"log"
+	"myapp/rps"
 	"net/http"
 )
 
+// homePage & playRound r handlers, evry web handler always 1)takes an http.ResponseWriter & 2)it takes a pointer to an http.Request
 func homePage(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html")
 }
 
+func playRound(w http.ResponseWriter, r *http.Request) {
+	//calling the func PlayRound from rps.go
+	winner, computerChoice, roundResult := rps.PlayRound(1)
+	log.Println(winner, computerChoice, roundResult)
+}
+
 func main() {
+	http.HandleFunc("/play", playRound)
 	// "/" default page
 	http.HandleFunc("/", homePage)
 
@@ -19,7 +28,7 @@ func main() {
 }
 
 func renderTemplate(w http.ResponseWriter, page string) {
-	//parse our html file into the necessary format & send that to the browser
+	//parse our html file n2 da necessary format & send dat 2da browser
 	//t: for template, err: checks for an error
 	t, err := template.ParseFiles(page)
 	if err != nil {
